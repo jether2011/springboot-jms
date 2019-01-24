ed7c1f65ac9d1b8a5324fa484fbfeaf76670b70e
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-01-24 21:35:09 -0200

    start a springboot jms example

---
+++ src/test/java/com/jetherrodrigues/jms/JmsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jetherrodrigues.jms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
