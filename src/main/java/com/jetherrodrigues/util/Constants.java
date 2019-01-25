2baf7ac22d64a25e161633bb5d092d37d9b695f9
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-01-24 22:51:36 -0200

    add constants to util package

---
+++ src/main/java/com/jetherrodrigues/util/Constants.java
@@ -0,0 +1,25 @@
+package com.jetherrodrigues.util;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Jether Rois
+ *
+ */
+public final class Constants implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * paths
+	 */
+	public static final String API = "api/";
+	public static final String V1 = "v1/";
+	
+	/**
+	 * resources
+	 */
+	public static final String ACQUISITION = "acquisition";
+	public static final String INSTRUMENT = "instrument";
+}
